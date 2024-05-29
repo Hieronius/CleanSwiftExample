ef39ad0c959e5a3304185ae5b5f098ee6e152614
Arsenty Halimovsky <ghost12@yandex.ru>
2024-05-29 06:35:41 +0300

    Add comment

--- CleanSwift Example/CleanSwift Example/Greeting/GreetingModels.swift
+++ CleanSwift Example/CleanSwift Example/Greeting/GreetingModels.swift
@@ -14,6 +14,7 @@ import UIKit
 
 // ViewModel
 typealias GreetingViewModel = Greeting.ShowGreeting.ViewModel
+// Response
 typealias GreetingResponse = Greeting.ShowGreeting.Response
 typealias GreetingRequest = Greeting.ShowGreeting.Request
 
